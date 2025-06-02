c276f65ff1fe7ec39ceac58b3999ad023400b70a
hahuy1708 <hahuy17082005@gmail.com>
2025-06-02 13:47:24 +0700

    add ShoppingCartService

---
+++ Service/ShoppingCartService.cs
@@ -0,0 +1,34 @@
+﻿using ECommerceApp.Data;
+using ECommerceApp.DTOs;
+using ECommerceApp.DTOs.ShoppingCartDTOs;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerceApp.Service
+{
+    public class ShoppingCartService
+    {
+        private readonly ApplicationDbContext _context;
+        public ShoppingCartService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<ApiResponse<CartResponseDTO>> GetCartByCustomerIdAsync(int id)
+        {
+            try
+            {
+                var cart = _context.Carts
+                    .Include(c => c.CartItems)
+                    .ThenInclude(ci => ci.Product)
+                    .FirstOrDefaultAsync(c => c.CustomerId == id && !c.IsCheckedOut);
+                if (cart == null)
+                {
+
+                }
+            }
+            catch(Exception ex)
+            {
+
+            }
+        }
+    }
+}
